609679fa15ca16e73b45599d280311dbe97643d8
ytet5uy4 <ytet5uy4@outlook.com>
2015-01-15 15:00:51 +0900

    tune up

--- Tools/update.sh
+++ Tools/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 update() {
     printf "Checking repository..."
-    git -C ~/.dotfiles pull > /dev/null
+    git -C ~/.dotfiles pull origin master > /dev/null 2>&1
     echo -e "\e[1;34mdone\e[m"
     bash ~/.dotfiles/Tools/create_symlink.sh
 
