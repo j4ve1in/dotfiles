19bf7ce4db1466355b4078ab9be9763b2ec1d062
ytet5uy4 <ytet5uy4@outlook.com>
2015-01-17 01:14:32 +0900

    tune up

--- Tools/update.sh
+++ Tools/update.sh
@@ -4,6 +4,8 @@ update() {
     git -C ~/.dotfiles pull origin master > /dev/null 2>&1
     echo -e "\e[1;34mdone\e[m"
     bash ~/.dotfiles/Tools/create_symlink.sh
+    echo "Vim Setting"
+    vim +NeoBundleUpdate +qall
 
     echo -e "Updated\n"
 }
