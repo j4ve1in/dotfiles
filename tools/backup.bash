c0ab5f534d6a977e2b5da66079de56d293b7cdd7
ytet5uy4 <ytet5uy4@outlook.com>
2015-02-27 23:13:49 +0900

    fix backup bug

--- tools/backup.bash
+++ tools/backup.bash
@@ -8,10 +8,12 @@ echo -e "\033[1;36mdone\033[0;39m"
 
 echo "Starting backup"
 
+# File
+echo "File"
 # Calc max
 max=0
 for dir_list in ${DIR_LIST[@]}; do
-    filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -name ".*")
+    filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type f -name ".*")
     for file in ${filename[@]}; do
         if [ -e ~/${file##*/} ]; then
             max=$(expr ${max} + 1)
@@ -20,7 +22,7 @@ for dir_list in ${DIR_LIST[@]}; do
 done
 
 for file in ${EXCEPTIONFILE_LIST[@]}; do
-    if [ -e ~/${file} ]; then
+    if [ -f ~/${file} ]; then
         max=$(expr ${max} + 1)
     fi
 done
@@ -28,21 +30,61 @@ done
 # Create
 count=0
 for dir_list in ${DIR_LIST[@]}; do
-    filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -name ".*")
+    filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type f -name ".*")
     for file in ${filename[@]}; do
         if [ -e ~/${file##*/} ]; then
             count=$(expr ${count} + 1)
             printf "[%3d/%3d] Copying: %s\n" ${count} ${max} ~/${file##*/}
-            cp -r ~/${file##*/} ~/.dotfiles_backup/${DATE}
+            cp ~/${file##*/} ~/.dotfiles_backup/${DATE}
         fi
     done
 done
 
 for file in ${EXCEPTIONFILE_LIST[@]}; do
-    if [ -e ~/${file} ]; then
+    if [ -f ~/${file} ]; then
         count=$(expr ${count} + 1)
         printf "[%3d/%3d] Copying: %s\n" ${count} ${max} ~/${file}
-        cp -r ~/${file} ~/.dotfiles_backup/${DATE}
+        cp ~/${file} ~/.dotfiles_backup/${DATE}
+    fi
+done
+
+# Directory
+echo "Directory"
+# Calc max
+max=0
+for dir_list in ${DIR_LIST[@]}; do
+    filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type d -name ".*")
+    for file in ${filename[@]}; do
+        if [ -e ~/${file##*/} ]; then
+            max=$(expr ${max} + 1)
+        fi
+    done
+done
+
+for file in ${EXCEPTIONFILE_LIST[@]}; do
+    if [ -d ~/${file} ]; then
+        max=$(expr ${max} + 1)
+    fi
+done
+
+# Create
+count=0
+for dir_list in ${DIR_LIST[@]}; do
+    filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type d -name ".*")
+    for file in ${filename[@]}; do
+        if [ -e ~/${file##*/} ]; then
+            count=$(expr ${count} + 1)
+            printf "[%3d/%3d] Copying: %s\n" ${count} ${max} ~/${file##*/}
+            cp -r ~/${file##*/}/ ~/.dotfiles_backup/${DATE}
+        fi
+    done
+done
+
+for file in ${EXCEPTIONFILE_LIST[@]}; do
+    if [ -d ~/${file} ]; then
+        count=$(expr ${count} + 1)
+        printf "[%3d/%3d] Copying: %s\n" ${count} ${max} ~/${file}
+        cp -r ~/${file}/ ~/.dotfiles_backup/${DATE}
     fi
 done
 
