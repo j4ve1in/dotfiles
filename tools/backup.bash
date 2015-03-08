85f5f17d8785d30e6f679c577136f4f2234cf1c0
ytet5uy4 <ytet5uy4@outlook.com>
2015-03-08 22:21:11 +0900

    update backup.bash

--- tools/backup.bash
+++ tools/backup.bash
@@ -49,48 +49,52 @@ if [ ! "${filemax}" = "0" -o ! "${dirmax}" = "0" ]; then
     # Start backup
     echo "Starting backup"
     ## File
-    echo "File"
-    count=0
-    for dir_list in ${DIR_LIST[@]}; do
-        filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type f -name ".*")
-        for file in ${filename[@]}; do
-            if [ -e ~/${file##*/} ]; then
+    if [ ! "${filemax}" = "0" ]; then
+        echo "File"
+        count=0
+        for dir_list in ${DIR_LIST[@]}; do
+            filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type f -name ".*")
+            for file in ${filename[@]}; do
+                if [ -e ~/${file##*/} ]; then
+                    count=$(expr ${count} + 1)
+                    printf "[%3d/%3d] Copying: %s\n" ${count} ${filemax} ~/${file##*/}
+                    cp ~/${file##*/} ~/.dotfiles_backup/${DATE}
+                fi
+            done
+        done
+
+        for file in ${EXCEPTIONFILE_LIST[@]}; do
+            if [ -f ~/${file} ]; then
                 count=$(expr ${count} + 1)
-                printf "[%3d/%3d] Copying: %s\n" ${count} ${filemax} ~/${file##*/}
-                cp ~/${file##*/} ~/.dotfiles_backup/${DATE}
+                printf "[%3d/%3d] Copying: %s\n" ${count} ${filemax} ~/${file}
+                cp ~/${file} ~/.dotfiles_backup/${DATE}
             fi
         done
-    done
-
-    for file in ${EXCEPTIONFILE_LIST[@]}; do
-        if [ -f ~/${file} ]; then
-            count=$(expr ${count} + 1)
-            printf "[%3d/%3d] Copying: %s\n" ${count} ${filemax} ~/${file}
-            cp ~/${file} ~/.dotfiles_backup/${DATE}
-        fi
-    done
+    fi
 
     ## Directory
-    echo "Directory"
-    count=0
-    for dir_list in ${DIR_LIST[@]}; do
-        filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type d -name ".*")
-        for file in ${filename[@]}; do
-            if [ -e ~/${file##*/} ]; then
+    if [ ! "${dirmax}" = "0" ]; then
+        echo "Directory"
+        count=0
+        for dir_list in ${DIR_LIST[@]}; do
+            filename=$(find ~/.dotfiles/${dir_list} -maxdepth 1 -type d -name ".*")
+            for file in ${filename[@]}; do
+                if [ -e ~/${file##*/} ]; then
+                    count=$(expr ${count} + 1)
+                    printf "[%3d/%3d] Copying: %s\n" ${count} ${dirmax} ~/${file##*/}
+                    cp -r ~/${file##*/}/ ~/.dotfiles_backup/${DATE}
+                fi
+            done
+        done
+
+        for file in ${EXCEPTIONFILE_LIST[@]}; do
+            if [ -d ~/${file} ]; then
                 count=$(expr ${count} + 1)
-                printf "[%3d/%3d] Copying: %s\n" ${count} ${dirmax} ~/${file##*/}
-                cp -r ~/${file##*/}/ ~/.dotfiles_backup/${DATE}
+                printf "[%3d/%3d] Copying: %s\n" ${count} ${dirmax} ~/${file}
+                cp -r ~/${file}/ ~/.dotfiles_backup/${DATE}
             fi
         done
-    done
-
-    for file in ${EXCEPTIONFILE_LIST[@]}; do
-        if [ -d ~/${file} ]; then
-            count=$(expr ${count} + 1)
-            printf "[%3d/%3d] Copying: %s\n" ${count} ${dirmax} ~/${file}
-            cp -r ~/${file}/ ~/.dotfiles_backup/${DATE}
-        fi
-    done
+    fi
 
     echo -e "Backup Complete\n"
 else
