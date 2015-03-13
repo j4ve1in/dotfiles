ab85f12ee68de8c06ca53d56dd2f617cecac1da4
ytet5uy4 <ytet5uy4@outlook.com>
2015-03-13 19:25:57 +0900

    update deploy.bash

--- tools/deploy.bash
+++ tools/deploy.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
-mkdir -p ~/.local
-cp ~/.dotfiles/sample/.aliases.local.sample ~/.local/.aliases.local
-cp ~/.dotfiles/sample/.gitconfig.local.sample ~/.local/.gitconfig.local
+mkdir -p ~/.local.d
+cp ~/.dotfiles/sample/aliases.local.sample ~/.local.d/aliases.local
+cp ~/.dotfiles/sample/gitconfig.local.sample ~/.local.d/gitconfig.local
 echo "Git Settings"
 printf "Username: "
 read GIT_USERNAME
-sed -e "s/USERNAME/${GIT_USERNAME}/g" ~/.local/.gitconfig.local
+sed -e "s/USERNAME/${GIT_USERNAME}/g" ~/.local.d/gitconfig.local
 printf "Email Address: "
 read GIT_EMAILADDRESS
-sed -e "s/EMAILADDRESS/${GIT_EMAILADDRESS}/g" ~/.local/.gitconfig.local
+sed -e "s/EMAILADDRESS/${GIT_EMAILADDRESS}/g" ~/.local.d/gitconfig.local
 
 echo -e "Deployed\n"
 
