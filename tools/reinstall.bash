5146406a4ee7db2fd3e8c1542cbad49ac4e41649
ytet5uy4 <ytet5uy4@outlook.com>
2015-03-28 19:38:39 +0900

    update reinstall.bash

--- tools/reinstall.bash
+++ tools/reinstall.bash
@@ -7,11 +7,15 @@ case $ANSWER in
     "Y" | "y" | "Yes" | "yes" )
         export ASSUME_YES="1"
         source ~/.dotfiles/tools/uninstall.bash
+        printf "Downloading installer..."
         if [ $(which curl) ]; then
-            bash -c "$(curl -L dot.j4ve1in.com)"
+            export LESS="-cE"; { sleep 1; curl -o ~/j4ve1in_dotfiles_install.bash -L dot.j4ve1in.com; } | less
         elif [ $(which wget) ]; then
-            bash -c "$(wget -O - dot.j4ve1in.com)"
+            export LESS="-cE"; { sleep 1; wget -O ~/j4ve1in_dotfiles_install.bash dot.j4ve1in.com; } | less
         fi
+        echo -e "\033[1;36mdone\033[0;39m"
+        source ~/j4ve1in_dotfiles_install.bash
+        rm -f ~/j4ve1in_dotfiles_install.bash
         export ASSUME_YES=""
         echo -e "Reinstalled\n"
         ;;
