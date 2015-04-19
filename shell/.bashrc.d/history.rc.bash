4fb3ccd940e08e3367a39e0a41f4af3477c13615
ytet5uy4 <ytet5uy4@outlook.com>
2015-04-19 11:43:20 +0900

    add history.rc.bash and update hisory.rc.zsh

---
+++ shell/.bashrc.d/history.rc.bash
@@ -0,0 +1,5 @@
+export HISTFILE=~/.bash/.bash_history
+export HISTSIZE=10000
+export SAVEHIST=10000
+export HISTFILESIZE=10000
+export HISTTIMEFORMAT='%F %T '
--- shell/.zshrc.d/history.rc.zsh
+++ shell/.zshrc.d/history.rc.zsh
@@ -1,12 +1,10 @@
-HISTFILE=~/.zsh/.zsh_history # historyファイル
-HISTFILESIZE=1000000
-HISTSIZE=1000000 # ファイルサイズ
-SAVEHIST=1000000 # saveする量
-setopt hist_ignore_dups # 重複を記録しない
-setopt hist_reduce_blanks # スペース排除
-setopt share_history # 履歴ファイルを共有
-setopt EXTENDED_HISTORY # zshの開始終了を記録
-# 操作?
+export HISTFILE=~/.zsh/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=10000
+export HISTFILESIZE=10000
+setopt hist_reduce_blanks
+setopt share_history
+setopt EXTENDED_HISTORY
 autoload history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
