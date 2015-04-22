e3be20d53ba27c6db3dd07a042799043f6bd4b70
ytet5uy4 <ytet5uy4@outlook.com>
2015-04-22 16:13:26 +0900

    update zsh completion

--- shell/.zshrc.d/completion.rc.zsh
+++ shell/.zshrc.d/completion.rc.zsh
@@ -1,17 +1,54 @@
 autoload -Uz compinit
 compinit -u
-setopt autopushd
+setopt autopushd # auto directory pushd that you can get dirs list by cd -[tab]
 setopt pushd_ignore_dups
-setopt auto_cd
-function cd() {
-    builtin cd $@ && ls;
-}
-setopt list_packed
+setopt list_packed # compacked complete list display
 setopt list_types
-setopt correct
+setopt correct # command correct edition before each completion attempt
 
 #allow tab completion in the middle of a word
 setopt COMPLETE_IN_WORD
 
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format '%F{cyan}Completing %B%d%b%f'
+
+# auto_cd
+setopt auto_cd
+function chpwd() {
+    ls_abbrev
+}
+ls_abbrev() {
+    if [[ ! -r $PWD ]]; then
+        return
+    fi
+    # -a : Do not ignore entries starting with ..
+    # -C : Force multi-column output.
+    # -F : Append indicator (one of */=>@|) to entries.
+    local cmd_ls='ls'
+    local -a opt_ls
+    opt_ls=('-aCF' '--color=always')
+    case "${OSTYPE}" in
+        freebsd*|darwin*)
+            if type gls > /dev/null 2>&1; then
+                cmd_ls='gls'
+            else
+                # -G : Enable colorized output.
+                opt_ls=('-aCFG')
+            fi
+            ;;
+    esac
+
+    local ls_result
+    ls_result=$(CLICOLOR_FORCE=1 COLUMNS=$COLUMNS command $cmd_ls ${opt_ls[@]} | sed $'/^\e\[[0-9;]*m$/d')
+
+    local ls_lines=$(echo "$ls_result" | wc -l | tr -d ' ')
+
+    if [ $ls_lines -gt 10 ]; then
+        echo "$ls_result" | head -n 5
+        echo '...'
+        echo "$ls_result" | tail -n 5
+        echo "$(command ls -1 -A | wc -l | tr -d ' ') files exist"
+    else
+        echo "$ls_result"
+    fi
+}
