a938c8c7d92e530670b6037000273cba14a4f77f
ytet5uy4 <ytet5uy4@outlook.com>
2015-04-28 00:29:54 +0900

    bug fix

--- tools/install_plugin.bash
+++ tools/install_plugin.bash
@@ -4,7 +4,7 @@
 echo -e "\033[4;39mStarting install plugin\033[0;39m"
 ## install plugin manager
 printf "Installing plugin manager..."
-{ sleep 1; git submodule update --init; } | env LESS="-cE" less
+{ sleep 1; git -C ~/.dotfiles submodule update --init; } | env LESS="-cE" less
 echo -e "\033[1;36mdone\033[0;39m"
 ## install editor plugin
 printf "Installing vim plugin..."
