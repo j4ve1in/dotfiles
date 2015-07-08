6cdf2374406d974ba00643b32b88ada3d86d9bd9
ytet5uy4 <ytet5uy4@outlook.com>
2015-07-09 00:28:12 +0900

    add tmux plugin and settings

--- tools/install_plugin.bash
+++ tools/install_plugin.bash
@@ -4,7 +4,7 @@
 echo -e "\033[4;39mStarting install plugin\033[0;39m"
 ## install plugin manager
 printf "Installing plugin manager..."
-{ sleep 1; git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim;git clone git://github.com/zsh-users/antigen.git ~/.zsh/bundle/antigen; } | env LESS="-cE" less
+{ sleep 1; git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim;git clone git://github.com/zsh-users/antigen.git ~/.zsh/bundle/antigen; git clone git://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm; } | env LESS="-cE" less
 echo -e "\033[1;36mdone\033[0;39m"
 ## install editor plugin
 printf "Installing vim plugin..."
