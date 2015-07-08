81e9b3143e8d47155e7fc6e89dcbb949a990a88e
ytet5uy4 <ytet5uy4@outlook.com>
2015-07-08 23:25:17 +0900

    update dotfile.csv and remove load_exdotfile.bash

--- tools/load_exdotfile.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exdotfile
-i=0
-while read line; do
-  EXDOTFILE[$i]=$(echo $line | cut -d ',' -f 2 | sed -e 's/"//g' -)
-  ((i=i+1))
-done < ~/.dotfiles/tools/exdotfile.csv
-export EXDOTFILE
