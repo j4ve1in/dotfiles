e3b24958ca089ffad012b22c48b22d7fe6b649f8
ytet5uy4 <ytet5uy4@outlook.com>
2015-07-08 23:22:40 +0900

    update environment variable

--- shell/dot.shrc.d/env.rc.sh
+++ shell/dot.shrc.d/env.rc.sh
@@ -1,27 +1,37 @@
 export LANG="en_US.UTF-8"
-export KCODE="u"
 export TERM="xterm-256color"
 export PAGER="less"
 export LESS="-ciMNR"
 export WWW_BROWSER="w3m"
-export CYGWIN="winsymlinks"
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$HOME/.dotfiles/bin"
 
-# rbenv
+# Cygwin
+if [ $OSTYPE = cygwin ]; then
+  export CYGWIN="winsymlinks"
+fi
+
+# Node.js
+## Nodebrew
+export PATH="$HOME/.nodebrew/current/bin:$PATH"
+
+# Ruby
+export KCODE="u"
+## rbenv
 if [ -d ~/.rbenv ]; then
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
 fi
 
+# Editor
 if which vim >/dev/null 2>&1; then
-    export EDITOR="vim"
-    export FCEDIT="vim"
-    export SVN_EDITOR="vim"
+  export EDITOR="vim"
+  export FCEDIT="vim"
+  export SVN_EDITOR="vim"
 else
-    export EDITOR="vi"
-    export FCEDIT="vi"
-    export SVN_EDITOR="vi"
+  export EDITOR="vi"
+  export FCEDIT="vi"
+  export SVN_EDITOR="vi"
 fi
 
 # man
