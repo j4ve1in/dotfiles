cea659ae5c9c744716dc0b87bb70255371ded44f
ytet5uy4 <ytet5uy4@outlook.com>
2015-08-10 00:03:57 +0900

    update shell function

--- shell/dot.shrc.d/function.rc.sh
+++ shell/dot.shrc.d/function.rc.sh
@@ -2,7 +2,7 @@ ssh-add() {
   command ssh-add $@
   if [ $? != 0 ] && [ -z "$SSH_AUTH_SOCK" ] && [ -z "$SSH_AGENT_PID" ]; then
     printf "start ssh-aget (yes/no)? "
-    read ANSWER
+    read local ANSWER
 
     case $ANSWER in
       "Y" | "y" | "Yes" | "yes" )
@@ -16,8 +16,8 @@ ssh-add() {
 }
 
 set-urxvt-font-size() {
-  old_name=`grep -i '^\s*urxvt.font' ~/.Xdefaults | cut -d: -f2-`
-  new_name=`echo "$old_name" | sed 's/:\(pixel\)\?size=[0-9]\+/'":\1size=$1/"`
+  local old_name=`grep -i '^\s*urxvt.font' ~/.Xdefaults | cut -d: -f2-`
+  local new_name=`echo "$old_name" | sed 's/:\(pixel\)\?size=[0-9]\+/'":\1size=$1/"`
 
   [ -n "$TMUX" ] && printf '\ePtmux;\e'
   printf '\e]50;%s\007' "$new_name"
