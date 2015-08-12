6735a3f7ccbb5b9aefa27b463ed5e4729cc62623
ytet5uy4 <ytet5uy4@outlook.com>
2015-08-12 17:03:46 +0900

    update zsh settings

--- shell/dot.zsh/.zprofile
+++ shell/dot.zsh/.zprofile
@@ -1,31 +1,38 @@
 # Compile
+echo -e "\033[4;39mCheck zsh's configuration files\033[0;39m"
 ## ~/.zsh/.zshrc
 file=".zsh/.zshrc"
-if [ ! -f ~/${file}.zwc -o ~/${file} -nt ~/${file}.zwc ]; then
+if [ ! -f ~/${file}.zwc ] || [ ~/${file} -nt ~/${file}.zwc ]; then
+  echo " Compile ${file}..."
   zcompile ~/${file}
-  echo "Compiled ${file}"
+  echo -e "\033[1;36mdone\033[0;39m"
 fi
 
 ## ~/.zshrc.d/*.rc.zsh
 for file in .zshrc.d/*.rc.zsh; do
-  if [ ! -f ~/${file}.zwc -o ~/${file} -nt ~/${file}.zwc ]; then
+  if [ ! -f ~/${file}.zwc ] || [ ~/${file} -nt ~/${file}.zwc ]; then
+    echo " Compile ${file}..."
     zcompile ~/${file}
-    echo "Compiled ${file}"
+    echo -e "\033[1;36mdone\033[0;39m"
   fi
 done
 
 ## ~/.zshrc.function.d/*.rc.zsh
 for file in .zshrc.function.d/*.rc.zsh; do
-  if [ ! -f ~/${file}.zwc -o ~/${file} -nt ~/${file}.zwc ]; then
+  if [ ! -f ~/${file}.zwc ] || [ ~/${file} -nt ~/${file}.zwc ]; then
+    echo " Compile ${file}..."
     zcompile ~/${file}
-    echo "Compiled ${file}"
+    echo -e "\033[1;36mdone\033[0;39m"
   fi
 done
 
 ## ~/.zshrc.plugin.d/*.rc.zsh
 for file in .zshrc.plugin.d/*.rc.zsh; do
-  if [ ! -f ~/${file}.zwc -o ~/${file} -nt ~/${file}.zwc ]; then
+  if [ ! -f ~/${file}.zwc ] || [ ~/${file} -nt ~/${file}.zwc ]; then
+    echo " Compile ${file}..."
     zcompile ~/${file}
-    echo "Compiled ${file}"
+    echo -e "\033[1;36mdone\033[0;39m"
   fi
 done
+echo
+clear
--- shell/dot.zshrc.plugin.d/antigen.rc.zsh
+++ shell/dot.zshrc.plugin.d/antigen.rc.zsh
@@ -1,10 +1,54 @@
 ADOTDIR=$HOME/.zsh/bundle
+ANTIGEN_PLUGINS=(
+  "zsh-users/zaw"
+  "Tarrasch/zsh-bd"
+)
+ANTIGEN_FUNCTIONS=(
+  "mollifier/cd-bookmark"
+  "mollifier/cd-gitroot"
+)
+
+echo -e "\033[4;39mLoading Antigen\033[0;39m"
 source ~/.zsh/bundle/antigen/antigen.zsh
-# Plugin
-antigen bundle zsh-users/zaw
-antigen bundle Tarrasch/zsh-bd
-# Function
-antigen bundle mollifier/cd-bookmark
-antigen bundle mollifier/cd-gitroot
-antigen bundle zsh-users/zsh-completions src
+
+if [ -z "$(ls ~/.zsh/bundle/repos)" ]; then
+  # Plugin
+  for plugin in ${ANTIGEN_PLUGINS[@]}; do
+    printf " Downloading $plugin..."
+    { sleep 1; antigen bundle $plugin; } | env LESS="-cE" less
+    echo -e "\033[1;36mdone\033[0;39m"
+  done
+
+  # Function
+  for function in ${ANTIGEN_FUNCTIONS[@]}; do
+    printf " Downloading $function..."
+    { sleep 1; antigen bundle $function; } | env LESS="-cE" less
+    echo -e "\033[1;36mdone\033[0;39m"
+  done
+
+  # Others
+  printf " Downloading zsh-users/zsh-completions..."
+  { sleep 1; antigen bundle zsh-users/zsh-completions src; } | env LESS="-cE" less
+  echo -e "\033[1;36mdone\033[0;39m"
+else
+  # Plugin
+  for plugin in ${ANTIGEN_PLUGINS[@]}; do
+    printf " Downloading $plugin..."
+    antigen bundle $plugin
+    echo -e "\033[1;36mdone\033[0;39m"
+  done
+  # Function
+  for function in ${ANTIGEN_FUNCTIONS[@]}; do
+    printf " Downloading $function..."
+    antigen bundle $function
+    echo -e "\033[1;36mdone\033[0;39m"
+  done
+  # Others
+  printf " Downloading zsh-users/zsh-completions..."
+  antigen bundle zsh-users/zsh-completions src
+  echo -e "\033[1;36mdone\033[0;39m"
+fi
 antigen apply
+echo
+sleep 1
+clear
