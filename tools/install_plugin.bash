eeb885d449ddc6a6854923bf2ffdc0b4c694fa53
ytet5uy4 <ytet5uy4@outlook.com>
2015-08-12 13:10:15 +0900

    update install_plugin.bash

--- tools/install_plugin.bash
+++ tools/install_plugin.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Start install plugin
-echo -e "\033[4;39mStarting install plugin\033[0;39m"
-## install plugin manager
-printf "Installing plugin manager..."
-{ sleep 1; git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim;git clone git://github.com/zsh-users/antigen.git ~/.zsh/bundle/antigen; git clone git://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm; } | env LESS="-cE" less
-echo -e "\033[1;36mdone\033[0;39m"
-## install editor plugin
-printf "Installing vim plugin..."
-vim +NeoBundleInstall +q
-echo -e "\033[1;36mdone\033[0;39m"
-echo -e "Installed plugin\n"
+# Start download plugin manager
+echo -e "\033[4;39mStarting download plugin manager\033[0;39m"
+
+CSV_FILE=~/.dotfiles/tools/plugin-manager.csv
+declare -a NAME=($(cat $CSV_FILE | cut -d ',' -f 1 | sed -e 's/"//g' -e '1d'))
+declare -a URL=($(cat $CSV_FILE | cut -d ',' -f 2 | sed -e 's/"//g' -e '1d'))
+declare -a DIR=($(cat $CSV_FILE | cut -d ',' -f 3 | sed -e 's/"//g' -e '1d'))
+declare -i N=${#NAME[@]}
+
+for ((i=0;i<N;i++)); do
+  printf " Downloading ${NAME[i]}..."
+  { sleep 1; git clone ${URL[i]} ${DIR[i]}; } | env LESS="-cE" less
+  echo -e "\033[1;36mdone\033[0;39m"
+done
