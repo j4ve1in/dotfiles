ece7d1b244784f4ea3aae445700bbd45de0b65ab
ytet5uy4 <ytet5uy4@outlook.com>
2015-08-28 04:58:51 +0900

    update settings of shell for osx

--- shell/dot.shrc.d/aliases.rc.sh
+++ shell/dot.shrc.d/aliases.rc.sh
@@ -131,3 +131,90 @@ alias rm='rm -iv'
 alias cp='cp -iv'
 alias mv='mv -iv'
 alias mysql='mysql --pager="less -iFMnSX"'
+
+## Linux
+if [ `uname` = Linux ]; then
+  alias xdo='xdg-open'
+  alias xrr='xrandr'
+
+  # Browse
+  alias google='xdg-open https://www.google.co.jp/'
+  alias github='xdg-open https://github.com/'
+  alias wikipedia='xdg-open http://ja.wikipedia.org/'
+
+  # Clipboard
+  alias xs='xsel'
+  alias xc='xclip'
+
+  # Gnome
+  alias gnb='gnome-boxes'
+  alias gncl='gnome-clocks'
+  alias gnca='gnome-calculator'
+  alias gncc='gnome-control-center'
+  alias gnd='gnome-disks'
+  alias gnf='gnome-font-viewer'
+  alias gnh='gnome-help'
+  alias gns='gnome-software'
+  alias gnsm='gnome-system-monitor'
+  alias gntt='gnome-tweak-tool'
+  alias gnwp='gnome-web-photo'
+
+  # Screenshot
+  ## import
+  alias imp='import'
+
+  ## xwd
+  alias xwd='xwd | convert - screenshot.png; rm screenshot.xwd'
+
+  ## Gnome-screenshot
+  alias gns='gnome-screenshot'
+  alias gnsa='gnome-screenshot --area'
+  alias gnsad='gnome-screenshot --area --delay=3'
+  alias gnsw='gnome-screenshot --window'
+  alias gnswd='gnome-screenshot --window --delay=3'
+  alias gnsd='gnome-screenshot --delay=3'
+
+  ## scrot
+  alias scr='scrot'
+
+  # Terminal
+  alias gnt='gnome-terminal'
+  alias urxvt='urxvt256c-ml'
+  alias urxvt-font='set-urxvt-font-size'
+fi
+
+## Cygwin
+if [ "$OSTYPE" = "cygwin" ]; then
+  alias cy='mintty -t Cygwin -i /Cygwin-Terminal.ico -'
+  alias cyc='cygcheck'
+  alias cyp='cygpath'
+  alias cys='cygstart'
+  alias rcy='exec cygstart mintty -t "Cygwin" -i /Cygwin-Terminal.ico -'
+
+  alias e='cygstart .'
+  alias explorer='cygstart .'
+
+  # PowerShell
+  alias powershell='cygstart powershell'
+  alias pwsh='cygstart pwsh'
+
+  # Browse
+  alias google='cygstart https://www.google.co.jp/'
+  alias github='cygstart https://github.com/'
+  alias wikipedia='cygstart http://ja.wikipedia.org/'
+fi
+
+## OSX
+if [ `uname` = Darwin ]; then
+  alias ls='ls -F'
+  alias op='open'
+  alias f='open .'
+  alias finder='open .'
+  alias pbc='pbcopy'
+  alias pbp='pbpaste'
+
+  # Browse
+  alias google='open https://www.google.co.jp/'
+  alias github='open https://github.com/'
+  alias wikipedia='open http://ja.wikipedia.org/'
+fi
