f7e300cc284cd85aadd4bcde0e66d0cc33710aa4
ytet5uy4 <ytet5uy4@outlook.com>
2015-11-25 02:23:06 +0900

    add ssh-add-with-lpass of shell function

--- shell/dot.shrc.d/function.rc.sh
+++ shell/dot.shrc.d/function.rc.sh
@@ -1,18 +1,77 @@
 ssh-add() {
-  command ssh-add $@
-  if [ $? != 0 ] && [ -z "$SSH_AUTH_SOCK" ] && [ -z "$SSH_AGENT_PID" ]; then
-    printf "start ssh-aget (yes/no)? "
-    read local ANSWER
-
-    case $ANSWER in
-      "Y" | "y" | "Yes" | "yes" )
-        echo Start ssh-agent
-        eval `ssh-agent`
-        ;;
-      * )
-        ;;
-    esac
+  if [ -n "$1" ]; then
+    if [ "$1" = "-p" ] || [ "$1" = "--lpass" ]; then
+      ssh-add-with-lpass $2
+    elif ! command ssh-add $@; then
+      echo "  -p hostname Use lpass.\n"
+      if [ -z "$SSH_AUTH_SOCK" ] && [ -z "$SSH_AGENT_PID" ]; then
+        printf "start ssh-aget (yes/no)? "
+        read local ANSWER
+        case $ANSWER in
+          "Y" | "y" | "Yes" | "yes" )
+            echo Start ssh-agent
+            eval `ssh-agent`
+            ;;
+          * )
+            ;;
+        esac
+      fi
+    fi
   fi
+}
+
+ssh-add-with-lpass() {
+  main() {
+    if type ssh-add expect >/dev/null 2>&1; then
+      if [ -n "$1" ]; then
+        REMOTE_HOST=$1
+      else
+        echo "missing argument"
+        echo "usage: ssh-add -p [remote_hostname]\n"
+        return 1
+      fi
+
+      if type lpass >/dev/null 2>&1; then
+        if [ -e ~/.lpass/username ]; then
+          identity-add
+        else
+          echo "Error: You need to login lastpass."
+          printf "  Username: "
+          read LASTPASS_USERNAME
+          printf "  "
+          if ! lpass login $LASTPASS_USERNAME; then
+            return 1
+          fi
+          identity-add
+        fi
+      else
+        echo "Error: You need to install \`lpass\`."
+        return 1
+      fi
+    else
+      if ! type ssh-add expect >/dev/null 2>&1; then
+        echo "Error: You need to install \`ssh-add\` and \`expect\`."
+      elif type ssh-add >/dev/null 2>&1; then
+        echo "Error: You need to install \`ssh-add\`."
+      elif type expect >/dev/null 2>&1; then
+        echo "Error: You need to install \`expect\`."
+      fi
+    fi
+  }
+
+  identity-add() {
+    PASSPHRASE=$(lpass show --field=Passphrase ${REMOTE_HOST}\(${USER}@$(hostname -s)\))
+    SSH_KEY_PATH=${HOME}/.ssh/key/${REMOTE_HOST}
+    expect -c "
+    spawn ssh-add $SSH_KEY_PATH
+    expect \"Enter passphrase for ${SSH_KEY_PATH}:\"
+    send -- \"${PASSPHRASE}\n\"
+    expect \"$\"
+    exit 0
+    " >/dev/null 2>&1
+    echo "Identity added: $SSH_KEY_PATH"
+  }
+  main $@
 }
 
 set-urxvt-font-size() {
