1be9d87fe4863c95407ce39e8bfc57e6469df32e
ytet5uy4 <ytet5uy4@outlook.com>
2015-12-23 01:54:25 +0900

    update environment variable

--- shell/dot.shrc.d/env.sh
+++ shell/dot.shrc.d/env.sh
@@ -1,41 +1,13 @@
 export LANG="en_US.UTF-8"
 export TERM="xterm-256color"
 export PAGER="less"
-export LESS="-ciMNR"
 export WWW_BROWSER="w3m"
-[ -d ~/bin ] && export PATH="$PATH:$HOME/bin"
-[ -d ~/.local/bin ] && export PATH="$PATH:$HOME/.local/bin"
-[ -d ~/.dotfiles/bin ] && export PATH="$PATH:$HOME/.dotfiles/bin"
-[ -d /usr/local/heroku ] && export PATH="$PATH:/usr/local/heroku/bin"
-
-# Cygwin
-if [ "$OSTYPE" = "cygwin" ]; then
-  export CYGWIN="winsymlinks"
-fi
-
-# OSX
-if [ "$OSTYPE" != "cygwin" ]; then
-  if [ "`uname`" = "Darwin" ]; then
-    export LSCOLORS="gxfxcxdxbxegedabagacad"
-  fi
-fi
-
-# Node.js
-## Nodebrew
-[ -d ~/.nodebrew ] && export PATH="$HOME/.nodebrew/current/bin:$PATH"
 
 # Ruby
 if type ruby >/dev/null 2>&1; then
   export KCODE="u"
   export RUBYGEMS_GEMDEPS=-
 fi
-## rbenv
-[ -d ~/.rbenv ] && export PATH="$HOME/.rbenv/bin:$PATH"
-[ -d ~/.rbenv ] && eval "`rbenv init -`"
-
-# Go
-[ -d ~/.local/src/go ] && export GOPATH=$HOME/.local/src/go
-[ -n $GOPATH ] && export PATH=$PATH:$GOPATH/bin
 
 # VirtualBox
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
@@ -54,6 +26,7 @@ else
 fi
 
 # Less
+export LESS="-ciMNR"
 export LESS_TERMCAP_mb=`printf "\033[1;31m"`
 export LESS_TERMCAP_md=`printf "\033[1;31m"`
 export LESS_TERMCAP_me=`printf "\033[0m"`
@@ -61,3 +34,31 @@ export LESS_TERMCAP_se=`printf "\033[0m"`
 export LESS_TERMCAP_so=`printf "\033[1;44m"`
 export LESS_TERMCAP_ue=`printf "\033[0m"`
 export LESS_TERMCAP_us=`printf "\033[1;36m"`
+
+# Path
+[ -d ~/.local/bin ] && export PATH="$PATH:$HOME/.local/bin"
+[ -d ~/.dotfiles/bin ] && export PATH="$PATH:$HOME/.dotfiles/bin"
+[ -d /usr/local/heroku ] && export PATH="$PATH:/usr/local/heroku/bin"
+
+## Node.js
+### Nodebrew
+[ -d ~/.nodebrew ] && export PATH="$HOME/.nodebrew/current/bin:$PATH"
+
+## Ruby
+### rbenv
+[ -d ~/.rbenv ] && export PATH="$HOME/.rbenv/bin:$PATH"
+[ -d ~/.rbenv ] && eval "`rbenv init -`"
+
+## Go
+[ -d ~/.local/src/go ] && export GOPATH=$HOME/.local/src/go
+[ -n $GOPATH ] && export PATH=$PATH:$GOPATH/bin
+
+# Others
+case "$(uname -s)" in
+  CYGWIN*|MSYS*)
+    export CYGWIN="winsymlinks"
+    ;;
+  Darwin)
+    export LSCOLORS="gxfxcxdxbxegedabagacad"
+    ;;
+esac
