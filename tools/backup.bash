450e6093125c5d4872418cebe41563e335eeae65
ytet5uy4 <ytet5uy4@outlook.com>
2016-03-22 20:34:01 +0900

    update backup.bash

--- tools/backup.bash
+++ tools/backup.bash
@@ -4,21 +4,37 @@ source ~/.dotfiles/tools/lib/base.bash
 source_dotool lib/dot
 
 cprint "Backup dotfiles" $UNDERLINE
+cd "$DOT_DIR"
 export GIT_{AUTHOR,COMMITTER}_NAME='dotsetup'
 export GIT_{AUTHOR,COMMITTER}_EMAIL='dotsetup@example.com'
-git -C "$DOT_DIR" stash >/dev/null 2>&1
-git -C "$DOT_DIR" branch | grep backup >/dev/null 2>&1
-if [ "$?" = "0" ]; then
-  git -C "$DOT_DIR" checkout backup >/dev/null 2>&1
-elif [ "$?" = "1" ]; then
-  git -C "$DOT_DIR" checkout --orphan backup
+git stash >/dev/null 2>&1
+git checkout master >/dev/null 2>&1
+
+# create branch
+git branch | grep backup >/dev/null 2>&1
+if [ "$?" = "1" ]; then
+  git checkout --orphan backup
+elif [ "$?" = "0" ]; then
+  BACKUP_BRANCH="backup/$(date +'%Y%m%d%H%M%S')"
+  git checkout --orphan "$BACKUP_BRANCH"
 fi
-git -C "$DOT_DIR" add -A
-git -C "$DOT_DIR" commit -m "backup dotfiles $(date +'at %H:%M:%S on %B the %d, %Y')" | head -n 10
+
+# add and commit
+git add -A
+git commit -m "backup dotfiles $(date +'at %H:%M:%S on %B the %d, %Y')" | head -n 10
 echo 'Try the following command for more information.'
 echo "  \$ cd $DOT_DIR"
 echo "  \$ git checkout backup && git log && git checkout -"
-git -C "$DOT_DIR" checkout master >/dev/null 2>&1
-git -C "$DOT_DIR" stash pop >/dev/null 2>&1
+
+# merge and remove backup branch
+if [ -n "$BACKUP_BRANCH" ]; then
+  git checkout backup >/dev/null 2>&1
+  git merge --no-ff "$BACKUP_BRANCH"
+  git branch -D "$BACKUP_BRANCH"
+fi
+
+git checkout master >/dev/null 2>&1
+git stash pop >/dev/null 2>&1
 unset GIT_{AUTHOR,COMMITTER}_NAME GIT_{AUTHOR,COMMITTER}_EMAIL
+cd - >/dev/null 2>&1
 echo
