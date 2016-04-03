a30dd5e4f21bf12b68497437cd73b1fb3abad910
ytet5uy4 <ytet5uy4@outlook.com>
2016-04-04 02:23:30 +0900

    update backup option of dotsetup

--- tools/backup.bash
+++ tools/backup.bash
@@ -12,12 +12,20 @@ git checkout master >/dev/null 2>&1
 
 # create branch
 BACKUP_BRANCH="backup/$(date +'%Y%m%d%H%M%S')"
+printf ' Creating backup branch...'
 git checkout --orphan "$BACKUP_BRANCH" >/dev/null 2>&1
+cprint 'done' $CYAN_B
+cprintf '  Backup branch: ' $COLOR_75_B
+echo "$BACKUP_BRANCH"
 
 # commit
-git commit -m "backup dotfiles" | head -n 10
-echo 'If you want to roll back, run the following command.'
-echo "  \$ git -C "$DOT_DIR" checkout $BACKUP_BRANCH"
+printf ' master'
+cprintf ' -> ' $COLOR_93_B
+print "$BACKUP_BRANCH"
+printf ' '
+git commit -m "backup dotfiles" | sed -n 2p
+echo ' If you want to roll back, run the following command.'
+echo "   $(cprintf '$' $COLOR_75_B) git -C "$DOT_DIR" checkout $BACKUP_BRANCH"
 
 git checkout master >/dev/null 2>&1
 git stash pop >/dev/null 2>&1
