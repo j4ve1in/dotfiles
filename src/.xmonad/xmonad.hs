9658c9638fcedce69b1ba05bda01f0dc0c485ecf
ytet5uy4 <ytet5uy4@outlook.com>
2016-05-01 19:56:05 +0900

    update keymap of xmonad

--- src/.xmonad/xmonad.hs
+++ src/.xmonad/xmonad.hs
@@ -178,7 +178,7 @@ main = do
             ((modm .|. controlMask, xK_l      ), spawn "systemctl suspend"),
             -- Launch dmenu for launching applicatiton
             ((modm                    , xK_r      ), spawn "exe=`dmenu_run -nb black -fn 'Migu 1M:size=13.5'` && exec $exe"),
-            ((modm .|. controlMask    , xK_p      ), spawn "power"),
+            ((modm                    , xK_p      ), spawn "power"),
             ((modm .|. shiftMask      , xK_g      ), spawn "gyazo"),
             -- Play / Pause media keys
             ((0                       , 0x1008ff18), spawn "sh $HOME/bin/cplay.sh"),
@@ -213,8 +213,8 @@ main = do
             ((modm                  , xK_t),     spawn "urxvtc --title htop -e htop"),
             -- Launch wps office
             ((modm .|. controlMask  , xK_w),     spawn "wps"),
-            ((modm                  , xK_e),     spawn "et"),
-            ((modm                  , xK_p),     spawn "wpp"),
+            ((modm .|. controlMask  , xK_e),     spawn "et"),
+            ((modm .|. controlMask  , xK_p),     spawn "wpp"),
             -- Launch File managers
             ((modm                  , xK_f),     spawn "urxvtc --title ranger --depth 0 -e ranger")
         ]
