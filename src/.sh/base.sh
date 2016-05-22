aa58287fc14baea1cf0274c12df9d2a6f04ffb57
ytet5uy4 <ytet5uy4@outlook.com>
2016-05-23 01:46:43 +0900

    bug fix

--- src/.sh/base.sh
+++ src/.sh/base.sh
@@ -19,6 +19,8 @@ fi
 # Launch tmux
 # if not inside a tmux session, and if no session is started,
 # start a new session
-has tmux && [ -z "$TMUX" ] && tmux attach -d >/dev/null 2>&1 || tmux
+if has tmux && [ -z "$TMUX" ]; then
+  tmux attach -d >/dev/null 2>&1 || tmux
+fi
 
 unset -f has cprintf
