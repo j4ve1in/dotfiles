edfe83b163eaf34038df853175b5842afdcfbf1e
ytet5uy4 <ytet5uy4@outlook.com>
2017-03-08 21:19:15 +0900

    Remove extend-ctrl-z

--- src/.zsh/rc/base.zsh
+++ src/.zsh/rc/base.zsh
@@ -26,7 +26,6 @@ bindkey -v '^[[Z' reverse-menu-complete
 bindkey -v '^[e' exec-editor
 bindkey -v '^[g' exec-git
 bindkey -v '^_' reload
-bindkey -v '^Z' extend-ctrl-z
 bindkey -v ' ' abbrev-expand
 bindkey -v '^x ' no-abbrev-expand
 
