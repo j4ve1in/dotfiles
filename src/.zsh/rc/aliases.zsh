e0696424705d827bd6eafc32f26c1f34954ee874
ytet5uy4 <ytet5uy4@ytet5uy4.com>
2017-05-18 13:13:32 +0900

    Add neofetch config file and update shell alias

--- src/.zsh/rc/aliases.zsh
+++ src/.zsh/rc/aliases.zsh
@@ -1,30 +1,21 @@
-has cp && cp='cp -iv'
-has less && alias le=less
+# regular alias
+has cp && alias c='cp -iv'
 has mv && alias m='mv -iv'
 has cd && alias -- -='cd -'
 has rm && alias rm='rm -iv'
 has ls && alias l='ls -F --color=always' ll='l -hl --time-style=long-iso'
 has ls && alias la='l -A' lla='ll -A'
-has grep && alias grep='grep --color=auto' fgrep='fgrep --color=auto'
-has grep && alias egrep='egrep --color=auto'
 has git && alias g=git
-has nvim && alias v='nvim -p'
+has nvim && alias n='nvim -p'
 has docker && alias d=docker
 has docker-compose && alias dc=docker-compose
 has pacman && alias p=pacman
 has yaourt && alias y=yaourt
-has ranger && disable r && alias f=ranger r=rifle
 has sudo && alias sudo='sudo ' s=sudo
 has sudoedit && alias sudoedit='sudoedit ' se=sudoedit
 has colordiff && alias di='colordiff -u'
-has screenfetch && alias info='neofetch --color_blocks off --ascii_colors 27 12'
-has inxi && alias info-full='inxi -Fz'
 has ssh && alias ssh='TERM=xterm-256color ssh'
-
-if has envchain; then
-  alias ec='envchain'
-  has aws && alias aws='envchain aws aws'
-fi
+has gomi && alias gm=gomi
 
 # Suffix alias
 alias -s sh=sh bash=bash
