71c7f72e181fa69ab591611c8e95a30d0c7bda8e
ytet5uy4 <ytet5uy4@ytet5uy4.com>
2017-05-30 18:20:28 +0900

    Update fzf-widgets settings

--- src/.zsh/rc/plugins.zsh
+++ src/.zsh/rc/plugins.zsh
@@ -56,17 +56,29 @@
       bindkey '^@'   fzf-select-widget
       bindkey '^@.'  fzf-edit-dotfiles
       bindkey '^@c'  fzf-change-directory
-      bindkey '^@d'  fzf-select-docker-widget
       bindkey '^@f'  fzf-edit-files
-      bindkey '^@g'  fzf-select-git-widget
-      bindkey '^@gc' fzf-git-change-repository
-      bindkey '^@gh' fzf-select-github-widget
       bindkey '^@k'  fzf-kill-processes
       bindkey '^@s'  fzf-exec-ssh
       bindkey '^\'   fzf-change-recent-directory
       bindkey '^r'   fzf-insert-history
       bindkey '^xd'  fzf-insert-directory
       bindkey '^xf'  fzf-insert-files
+
+      # Git
+      bindkey '^@g'  fzf-select-git-widget
+      bindkey '^@ga' fzf-git-add-files
+      bindkey '^@gc' fzf-git-change-repository
+
+      # GitHub
+      bindkey '^@h'  fzf-select-github-widget
+      bindkey '^@hs' fzf-github-show-issue
+      bindkey '^@hc' fzf-github-close-issue
+
+      # Docker
+      bindkey '^@d'  fzf-select-docker-widget
+      bindkey '^@dc' fzf-docker-remove-containers
+      bindkey '^@di' fzf-docker-remove-images
+      bindkey '^@dv' fzf-docker-remove-volumes
     fi
   fi
 }
