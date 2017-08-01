d3304a48edc984c50733c0956e99153ea90e760f
ytet5uy4 <ytet5uy4@ytet5uy4.com>
2017-08-01 11:02:35 +0900

    Update zsh alias

--- src/.zsh/rc/aliases.zsh
+++ src/.zsh/rc/aliases.zsh
@@ -1,21 +1,21 @@
 # Regular alias
-alias c=cp
-alias m=mv
-alias mkd=mkdir
 alias -- -='cd -'
-alias l=ls la='l -A' ll='l -l' lla='ll -A'
-alias g=hub
-alias n=nvim
+alias c=cp
 alias d=docker
 alias dc=docker-compose
+alias g=hub
+alias gm=gomi
+alias l=ls la='l -A' ll='l -l' lla='ll -A'
+alias m=mv
+alias md=mkdir
+alias n=nvim
 alias s='sudo '
 alias se=sudoedit
-alias gm=gomi
 
 # Suffix alias
 alias -s bash=bash
 alias -s go='go run'
-alias -s zsh=zsh
 alias -s vim=nvim
+alias -s zsh=zsh
 alias -s {gif,jpg,jpeg,png,bmp}=xdg-open
 alias -s {gz,tgz,zip,tar,rar,7z}=aunpack
