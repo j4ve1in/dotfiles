48efea7e1cd58cb0cb2755ffff12d668d6271d42
ytet5uy4 <ytet5uy4@ytet5uy4.com>
2017-11-04 04:09:56 +0900

    Update offlineimap and mutt settings

---
+++ src/.config/offlineimap/offlineimap.py
@@ -0,0 +1,21 @@
+
+#! /usr/bin/env python2
+import subprocess
+
+def get_credential(account, env):
+  return subprocess.check_output(
+    ["envchain", "mail/" + account + "/imap", "printenv", "IMAP_" + env],
+    universal_newlines=True
+  ).strip("\n")
+
+def get_host(account):
+  return get_credential(account, "HOST")
+
+def get_port(account):
+  return get_credential(account, "PORT")
+
+def get_username(account):
+  return get_credential(account, "USERNAME")
+
+def get_password(account):
+  return get_credential(account, "PASSWORD")
