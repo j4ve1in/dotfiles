a21e4143c153c9520248cf13c3a2252e63904bf1
ytet5uy4 <ytet5uy4@ytet5uy4.com>
2018-04-28 12:52:43 +0900

    Update for fish

--- src/.config/fish/functions/fish_user_key_bindings.fish
+++ src/.config/fish/functions/fish_user_key_bindings.fish
@@ -4,4 +4,6 @@ function fish_user_key_bindings
   bind -M insert \c_ __fzf_insert_files
   bind -M insert \cs __fzf_exec_snippet
   bind -M insert \cy accept-autosuggestion
+  bind -M insert \cp 'history --merge; up-or-search'
+  bind -M insert \cn 'history --merge; down-or-search'
 end
